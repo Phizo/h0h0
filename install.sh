d2231d76f131a5e9fc91307b17149379606cd377
Phizo <wfs225-dg@hotmail.com>
2014-07-21 23:29:30 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 OUTFILE="$PWD/h0h0.so"
 
 if [ "$(id -u)" != "0" ]; then
-	echo "You need to be root, ya dard.\n" 1>&2
-	exit 1
+    echo "You need to be root, ya dard.\n" 1>&2
+    exit 1
 fi
 
 make
 
 if [ -f $OUTFILE ]; then
-	echo $OUTFILE > /etc/ld.so.preload
-	echo "Done.\n";
+    echo $OUTFILE > /etc/ld.so.preload
+    echo "Done.\n";
 else
-	echo "** $OUTFILE not found; cannot write to /etc/ld.so.preload. **\n" 1>&2;
+    echo "** $OUTFILE not found; cannot write to /etc/ld.so.preload. **\n" 1>&2;
 fi
