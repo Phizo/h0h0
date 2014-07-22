dd202e0d5ec379d95ba7633a5ee85279223633b4
Phizo <wfs225-dg@hotmail.com>
2014-07-23 02:07:24 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 OUTFILE="$PWD/h0h0.so"
+SUCCESS='\e[1m\e[32msuccessful\e[0m.'
+FAILED='\e[1m\e[31mfailed\e[0m.'
 
-if [ "$(id -u)" != "0" ]; then
-    echo "You need to be root, ya dard.\n" 1>&2
-    exit 1
+if [ "$(id -u)" != '0' ]; then
+	echo 'You need to be root, ya dard.' 1>&2
+	exit 1
 fi
 
+# I'm an eye-candy kinda guy...
+echo -e '\n\e[1m\e[31m[ Configuration ]\e[0m'
+echo '-------------------'
+egrep '^\s*#define' config.h | awk '{printf "%s: %s\n", $2, $3}'
+echo -e '-------------------\n\n'
+
+make clean
 make
 
 if [ -f $OUTFILE ]; then
-    echo $OUTFILE > /etc/ld.so.preload
-    echo "Done.\n";
+	echo $OUTFILE > /etc/ld.so.preload
+	echo -e "[+] Installation $SUCCESS\n";
 else
-    echo "** $OUTFILE not found; cannot write to /etc/ld.so.preload. **\n" 1>&2;
+	echo "[+] ** $OUTFILE not found; cannot write to /etc/ld.so.preload. **\n" 1>&2;
+	echo -e "[+] Installation $FAILED\n";
 fi
